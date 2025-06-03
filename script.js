f77fa3e1ddb62ded6a1e4b273a1f8cf35c5cd35e
Lucarim <lucas--amorim@outlook.com>
2025-06-02 21:00:16 -0300

    animacao-suave

--- script.js
+++ script.js
@@ -34,3 +34,21 @@ function initAcoordion() {
   }
 }
 initAcoordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+  if (linksInternos.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    linksInternos.forEach((i) => {
+      i.addEventListener("click", scrollToSection);
+    });
+  }
+}
+initScrollSuave();
