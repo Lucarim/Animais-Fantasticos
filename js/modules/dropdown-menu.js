afaaeefb5dcd39760512d75940d77ada9df288ea
Lucarim <lucas--amorim@outlook.com>
2025-07-04 16:54:24 -0300

    clickOutSide

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,11 +1,24 @@
 export default function initDropdownMenu() {
   const dropdownMenu = document.querySelector("[data-dropdown]");
+  const html = document.documentElement;
 
   ["touchstart", "click"].forEach((userEvent) => {
     dropdownMenu.addEventListener(userEvent, handleClick);
   });
+
   function handleClick(event) {
     event.preventDefault();
-    this.classList.toggle("ativo");
+    this.classList.add("ativo");
+    html.addEventListener("click", clickOutSide);
+    html.addEventListener("touchstart", clickOutSide);
+  }
+
+  function clickOutSide(event) {
+    console.log(event.target);
+    if (!dropdownMenu.contains(event.target)) {
+      dropdownMenu.classList.remove("ativo");
+      html.removeEventListener("click", clickOutSide);
+      html.removeEventListener("touchstart", clickOutSide);
+    }
   }
 }
