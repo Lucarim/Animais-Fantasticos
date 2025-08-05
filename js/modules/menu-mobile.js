4c635742c65544140f28575352f20e4929ae4841
Lucarim <lucas--amorim@outlook.com>
2025-08-05 15:23:46 -0300

    cancelar correcao anterior e corrigir fetch

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,7 +3,7 @@ import outSideClick from "./outsideclick.js";
 export default function initMenuMobile() {
   const menuButton = document.querySelector("[data-menu='button']");
   const menuList = document.querySelector("[data-menu='list']");
-  const eventos = ["click"];
+  const eventos = ["click", "touchstart"];
 
   if (menuButton) {
     function openMenu() {
