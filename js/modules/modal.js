c95031ba56ce6f31438d20ace3816407da22df4b
Lucarim <lucas--amorim@outlook.com>
2025-08-08 18:15:56 -0300

    modal transformado em classe

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,17 +1,36 @@
-export default function initModal() {
-  const modal = document.querySelector("[data-modal='container']");
-  const abrirModal = document.querySelector("[data-modal='abrir']");
-  const fecharModal = document.querySelector("[data-modal='fechar']");
-  function toggleModal(event) {
+export default class Modal {
+  constructor(modal, abrirModal, fecharModal) {
+    this.modal = document.querySelector(modal);
+    this.abrirModal = document.querySelector(abrirModal);
+    this.fecharModal = document.querySelector(fecharModal);
+
+    //bind this ao callback para fazer referencia ao objeto da classe
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
+  }
+  // abre ou fecha o modal
+  toggleModal() {
+    this.modal.classList.toggle("ativo");
+  }
+  //adiciona o evento de toggle ao modal
+  eventToggleModal(event) {
     event.preventDefault();
-    modal.classList.toggle("ativo");
+    this.toggleModal();
   }
-  function cliqueForaModal(event) {
-    if (event.target === this) toggleModal(event);
+  // adiciona os eventos de clique
+  addEventModal() {
+    this.abrirModal.addEventListener("click", this.eventToggleModal);
+    this.fecharModal.addEventListener("click", this.eventToggleModal);
+    this.modal.addEventListener("click", this.cliqueForaModal);
   }
-  if (modal && abrirModal && fecharModal) {
-    abrirModal.addEventListener("click", toggleModal);
-    fecharModal.addEventListener("click", toggleModal);
-    modal.addEventListener("click", cliqueForaModal);
+  //inicia a classe
+  init() {
+    if (this.modal && this.abrirModal && this.fecharModal) {
+      this.addEventModal();
+    }
+  }
+  //fecha modal ao clicar fora
+  cliqueForaModal(event) {
+    if (event.target === this.modal) this.toggleModal();
   }
 }
