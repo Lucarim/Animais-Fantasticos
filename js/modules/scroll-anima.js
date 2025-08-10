3923885f69136139837f392ae3603fe9484344e0
Lucarim <lucas--amorim@outlook.com>
2025-08-10 15:40:45 -0300

    animação ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,24 +1,41 @@
 export default class ScrollAnima {
   constructor(section) {
     this.sections = document.querySelectorAll(section);
-    this.windowMetade = window.innerHeight * 0.7;
+    this.windowMetade = window.innerHeight * 0.5;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
-
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.windowMetade < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  // pega a distancia de cada elemento em relação ao topo da pagina
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+  //checa a distancia do elemento ao scroll da pagina
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else {
+        item.element.classList.remove("ativo");
       }
     });
   }
+  //adiciona o evento de animação
   init() {
-    window.addEventListener("scroll", this.animaScroll);
-    this.animaScroll();
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+  //remove o evento de animação
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
